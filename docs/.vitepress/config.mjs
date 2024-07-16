ce44fd5604292fca62f2d453a31c58a6edb12d80
Nguyen Le Y <ley995@live.com>
2024-07-16 13:08:51 +0700

    favicon update

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -3,13 +3,13 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "leybme",
-  description: " A dedicated platform by Nguyen Le Y for sharing knowledge and experiences in engineering, focusing on embedded programming, hardware design, and biomedical instrumentation.",
+  description: " A site by Nguyen Le Y for sharing knowledge and experiences in engineering, focusing on embedded programming, hardware design, and biomedical instrumentation.",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Project', link: '/markdown-examples' },
-      { text: 'Blogs', link: '/markdown-examples' },
+      { text: 'Project', link: '/rocker' },
+      { text: 'Blogs', link: '/blogs' },
     ],
 
     sidebar: [
@@ -29,5 +29,6 @@ export default defineConfig({
       { icon: 'facebook', link: 'https://facebook.com/ley995' },
       { icon: 'linkedin', link: 'https://www.linkedin.com/in/nguyenley' },
     ]
-  }
+  },
+  head: [['link', { rel: 'icon', href: '/images/favicon.ico' }]]
 })
